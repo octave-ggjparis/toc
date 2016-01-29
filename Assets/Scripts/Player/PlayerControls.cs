db54733416d2aa4fb35e7b6d3128805b359b80e4
Guillaume Olivetti <contact@devduweb.com>
2016-01-30 00:09:06 +0100

    Character controls

---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+    [SerializeField]
+    private float maxSpeed = 10f;
+
+    [SerializeField]
+    private Rigidbody2D selfRigidBody;
+
+    [SerializeField]
+    private Transform selfTransform;
+
+    private enum direction
+    {
+        LEFT,
+        RIGHT
+    }
+
+    private Vector3 initialScale;
+    private direction currentDirection = direction.LEFT;
+
+	void Awake ()
+    {
+        initialScale = selfTransform.localScale;
+	}
+	
+	void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal > 0.25f || horizontal < 0.25f)
+        {
+            move(horizontal);
+        }
+	}
+
+    private void move(float horizontal)
+    {
+        selfRigidBody.AddForce(new Vector2(25 * maxSpeed * horizontal * Time.deltaTime, 0f));
+        selfRigidBody.velocity = new Vector2(
+            Mathf.Clamp(selfRigidBody.velocity.x, -maxSpeed, maxSpeed),
+            0
+        );
+
+        if(
+            (horizontal < 0 && currentDirection == direction.RIGHT) ||
+            (horizontal > 0 && currentDirection == direction.LEFT)
+        )
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        currentDirection = (currentDirection == direction.LEFT) ? direction.RIGHT : direction.LEFT;
+
+        selfTransform.localScale = new Vector3(
+            (currentDirection == direction.LEFT) ? initialScale.x : -initialScale.x,
+            initialScale.y,
+            1
+        );
+
+    }
+}
