03349e40623c6fe53a2febeb14e03dddf636d951
Guillaume Olivetti <contact@devduweb.com>
2016-01-30 16:55:12 +0100

    day/night

---
+++ Assets/Scripts/Interactables/LightSwitch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour {
+
+    [SerializeField]
+    private SpriteRenderer[] affectedSprite;
+
+    [SerializeField]
+    private Material LitMaterial;
+
+    [SerializeField]
+    private Material DarkMaterial;
+
+    public void toggleLight()
+    {
+        foreach(SpriteRenderer itemRender in affectedSprite)
+        {
+            if (itemRender.material.name == "Day (Instance)")
+            {
+                itemRender.material = DarkMaterial;
+            } else
+            {
+                itemRender.material = LitMaterial;
+            }
+        }
+    }
+}
