d0117114c265cc3b2727386e21ca702c6df9bb30
Guillaume Olivetti <contact@devduweb.com>
2016-01-31 13:35:49 +0100

    failstate

--- Assets/Scripts/Player/FailureState.cs
+++ Assets/Scripts/Player/FailureState.cs
@@ -1,17 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.ImageEffects;
+using UnityEngine.SceneManagement;
 
 public class FailureState : MonoBehaviour {
 
     [SerializeField]
     private GameObject mainCamera;
+    [SerializeField]
+    private VignetteAndChromaticAberration vignette;
 
     private bool isStressed = false;
+    private bool failure = false;
 
     private float microtime = 0;
 
     public void Update()
     {
+        if(failure)
+        {
+            if(vignette.intensity < 1f)
+            {
+                vignette.intensity = Mathf.Clamp(vignette.intensity + (Time.deltaTime / 1.5f), 0f, 1f);
+            } else
+            {
+                SceneManager.LoadScene(Application.loadedLevel);
+            }
+        }
+
         microtime += Time.deltaTime * 1.5f;
         microtime = microtime % 1000;
 
@@ -38,6 +54,13 @@ public class FailureState : MonoBehaviour {
 
     public void stress()
     {
+        if(isStressed)
+        {
+            Debug.Log("failState");
+            failure = true;
+            return;
+        }
+        
         isStressed = true;
     }
 }
